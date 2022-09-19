33668a549fc4199955ebbbd87f26f1d2b980596e
Jessica Paul <pauljessica82@gmail.com>
2022-09-18 23:05:54 -0400

    add backend

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#2@zs3g4@@zbfb%v-3f$uf)m%yfcgnk9_%*^t%3tmz-6zfbx!l'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
