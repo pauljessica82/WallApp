c8cf4592c34782fff9613b4d201498215b8e4b3a
Jessica Paul <pauljessica82@gmail.com>
2022-09-23 08:52:30 -0400

    settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,12 +7,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('SECRET_KEY')
-SECRET_KEY = "#2@zs3g4@@zbfb%v-3f$uf)m%yfcgnk9_%*^t%3tmz-6zfbx!l"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.getenv('SECRET_KEY', 'wallapp-secretkey' if DEBUG else None)
 
 ALLOWED_HOSTS = []
 
